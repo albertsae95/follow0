dd7f8dc0dffac1287dc98eba36889176744175dd
albertsae95 <albert.swierczynski95@gmail.com>
2018-06-08 14:32:16 +0200

    Transfer from IplImage to Mat in int main()

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <opencv2/video/tracking.hpp>
 #include "opencv2/opencv.hpp"
 #include <math.h>
+#include "opencv2/core/mat.hpp"
 
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/videoio/videoio_c.h"
@@ -16,7 +17,7 @@
 
 #include "utils.h"
 
-#define USE_VIDEO 1
+//#define USE_VIDEO 1
 
 using namespace cv;
 using namespace std;
@@ -25,6 +26,10 @@ using namespace std;
 #define RED CV_RGB(255,0,0)
 #define BLUE CV_RGB(255,0,255)
 #define PURPLE CV_RGB(255,0,255)
+
+#define K_VARY_FACTOR 0.2f
+#define B_VARY_FACTOR 20
+#define MAX_LOST_FRAMES 30
 
 struct Lane {
     Lane(){}
@@ -37,14 +42,25 @@ struct Lane {
     bool visited, found;
     float k, b;
 };
-/*
-void crop(IplImage* src,  IplImage* dst, CvRect rect) {
-    cvSetImageROI(src, rect);
-    cvCopy(src, dst);
-    cvSetImageCOI(src, 0);
+
+struct Status {
+    Status():reset(true),lost(0){}
+    ExpMovingAverage k, b;
+    bool reset;
+    int lost;
+};
+
+void crop(Mat src,  Mat dst, CvRect rect) {
+    //MatOp::roi(src, rect.y, rect.x, src);
+    //cvSetImageROI(src, rect);
+    dst = src(rect);
+    //src.copyTo(dst);
+    //cvSetImageCOI(src, 0);
     //cvResetImageROI(src);
 }
-*/
+
+Status laneR, laneL;
+
 enum{
     SCAN_STEP = 5,			  // in pixels
     LINE_REJECT_DEGREES = 10, // in degrees
@@ -60,7 +76,146 @@ enum{
     HOUGH_MAX_LINE_GAP = 100  // join lines to one with smaller than this gaps
 };
 
-void processLanes(CvSeq* lines, IplImage* edges, IplImage* temp_frame) {
+void FindResponses(IplImage *img, int startX, int endX, int y, std::vector<int>& list)
+{
+    // scans for single response: /^\_
+
+    const int row = y * img->width * img->nChannels;
+    unsigned char* ptr = (unsigned char*)img->imageData;
+
+    int step = (endX < startX) ? -1: 1;
+    int range = (endX > startX) ? endX-startX+1 : startX-endX+1;
+
+    for(int x = startX; range>0; x += step, range--)
+    {
+        if(ptr[row + x] <= BW_TRESHOLD) continue; // skip black: loop until white pixels show up
+        printf("imagedata=%u\n", *ptr);
+        // first response found
+        int idx = x + step;
+
+        // skip same response(white) pixels
+        while(range > 0 && ptr[row+idx] > BW_TRESHOLD){
+            idx += step;
+            range--;
+        }
+
+        // reached black again
+        if(ptr[row+idx] <= BW_TRESHOLD) {
+            printf("imagedata=%u\n", *ptr);
+            list.push_back(x);
+        }
+
+        x = idx; // begin from new pos
+    }
+}
+
+void processSide(std::vector<Lane> lanes, IplImage *edges, bool right) {
+
+    Status* side = right ? &laneR : &laneL;
+
+    // response search
+    int w = edges->width;
+    int h = edges->height;
+    const int BEGINY = 0;
+    const int ENDY = h-1;
+    const int ENDX = right ? (w-BORDERX) : BORDERX;
+    int midx = w/2;
+    int midy = edges->height/2;
+    unsigned char* ptr = (unsigned char*)edges->imageData;
+
+    // show responses
+    int* votes = new int[lanes.size()]; //considering all the lines that we got
+    for(int i=0; i<lanes.size(); i++) votes[i++] = 0;
+
+    for(int y=ENDY; y>=BEGINY; y-=SCAN_STEP) {  //from beginy to endy scanning
+        std::vector<int> rsp;
+        FindResponses(edges, midx, ENDX, y, rsp);
+
+        if (rsp.size() > 0) {
+            int response_x = rsp[0]; //use first response (closest to screen center)
+
+            float dmin = 9999999;
+            float xmin = 9999999;
+            int match = -1;
+            for (int j=0; j<lanes.size(); j++) {
+                // compute response point distance to current line
+                float d = dist2line(
+                        cvPoint2D32f(lanes[j].p0.x, lanes[j].p0.y),
+                        cvPoint2D32f(lanes[j].p1.x, lanes[j].p1.y),
+                        cvPoint2D32f(response_x, y));
+
+                // point on line at current y line
+                int xline = (y - lanes[j].b) / lanes[j].k;
+                int dist_mid = abs(midx - xline); // distance to midpoint
+
+                // pick the best closest match to line & to screen center
+                if (match == -1 || (d <= dmin && dist_mid < xmin)) {
+                    dmin = d;
+                    match = j;
+                    xmin = dist_mid;
+                    break;
+                }
+            }
+
+            // vote for each line
+            if (match != -1) {
+                votes[match] += 1;
+                printf("current_votes=%d\n", *votes);
+            }
+        }
+    }
+
+    int bestMatch = -1;
+    int mini = 9999999;
+    for (int i=0; i<lanes.size(); i++) {
+        int xline = (midy - lanes[i].b) / lanes[i].k;
+        int dist = abs(midx - xline); // distance to midpoint
+
+        if (bestMatch == -1 || (votes[i] > votes[bestMatch] && dist < mini)) {
+            bestMatch = i;
+            mini = dist;
+        }
+    }
+
+    if (bestMatch != -1) {
+        Lane* best = &lanes[bestMatch]; //assign ID of the best line
+        float k_diff = fabs(best->k - side->k.get());
+        float b_diff = fabs(best->b - side->b.get());
+
+        bool update_ok = (k_diff <= K_VARY_FACTOR && b_diff <= B_VARY_FACTOR) || side->reset;
+
+        printf("side: %s, k vary: %.4f, b vary: %.4f, lost: %s\n",
+            (right?"RIGHT":"LEFT"), k_diff, b_diff, (update_ok?"no":"yes"));
+
+        if (update_ok) {
+            // update is in valid bounds
+            side->k.add(best->k);
+            side->b.add(best->b);
+            side->reset = false;
+            side->lost = 0;
+        } else {
+            // can't update, lanes flicker periodically, start counter for partial reset!
+            side->lost++;
+            if (side->lost >= MAX_LOST_FRAMES && !side->reset) {
+                side->reset = true;
+            }
+        }
+
+    } else {
+        printf("no lanes detected - lane tracking lost! counter increased\n");
+        side->lost++;
+        if (side->lost >= MAX_LOST_FRAMES && !side->reset) {
+            // do full reset when lost for more than N frames
+            side->reset = true;
+            side->k.clear();
+            side->b.clear();
+        }
+    }
+
+    delete[] votes;
+}
+
+void processLanes(std::vector<Lane> lines, IplImage* edges, IplImage* temp_frame) {
 
     // classify lines to left/right side
     std::vector<Lane> left, right;
@@ -100,10 +255,9 @@ void processLanes(CvSeq* lines, IplImage* edges, IplImage* temp_frame) {
         cvLine(temp_frame, left[i].p0, left[i].p1, CV_RGB(255, 0, 0), 2);
     }
 
-    //processSide(left, edges, false);
-   // processSide(right, edges, true);
+    processSide(left, edges, false);
+    processSide(right, edges, true);
 
-/*
     // show computed lanes
     int x = temp_frame->width * 0.55f;
     int x2 = temp_frame->width;
@@ -115,14 +269,14 @@ void processLanes(CvSeq* lines, IplImage* edges, IplImage* temp_frame) {
     cvLine(temp_frame, cvPoint(x, laneL.k.get()*x + laneL.b.get()),
         cvPoint(x2, laneL.k.get() * x2 + laneL.b.get()), CV_RGB(255, 0, 255), 2);
 }
-*/
- }
 
 int main(void)
 {
-    string path = "~/albertsae95/follow0/road.avi";
+    double rho = 1;
+    double theta = CV_PI/180;
+    string path = "/home/albert/albertsae95/follow0/road.avi";
     VideoCapture capture(path);
-
+    //argv[1];
 
      if (!capture.isOpened() ) {
         fprintf(stderr, "Error: Can't open video\n");
@@ -130,72 +284,79 @@ int main(void)
     }
 
 
-    Size video_size = Size ((int) CAP_PROP_FRAME_WIDTH,
-                            (int) CAP_PROP_FRAME_HEIGHT);
+    Size video_size(capture.get(CAP_PROP_FRAME_WIDTH), capture.get(CAP_PROP_FRAME_HEIGHT));
 
     //long current_frame = 0;
     int key_pressed = 0;
-    Mat *frame;
+    Mat frame;
 
     Size frame_size = Size(video_size.width, video_size.height/2);
-    IplImage *temp_frame = cvCreateImage(frame_size, IPL_DEPTH_8U, 3);
-    IplImage *grey = cvCreateImage(frame_size, IPL_DEPTH_8U, 1);
-    IplImage *edges = cvCreateImage(frame_size, IPL_DEPTH_8U, 1);
-    IplImage *half_frame = cvCreateImage(cvSize(video_size.width/2, video_size.height/2), IPL_DEPTH_8U, 3);
+    Mat temp_frame(frame_size, IPL_DEPTH_8U, 3);
+    Mat grey(frame_size, IPL_DEPTH_8U, 1);
+    Mat edges(frame_size, IPL_DEPTH_8U, 1);
+    Mat half_frame(video_size.width/2, video_size.height/2, IPL_DEPTH_8U, 3);
 
     CvMemStorage* houghStorage = cvCreateMemStorage(0);
 
     //cvSetCaptureProperty(input_video, CV_CAP_PROP_POS_FRAMES, current_frame);
     while(key_pressed != 27) {
 
-        capture.read(*frame);
-        //if (*frame == NULL){
-          //  fprintf(stderr, "Error: null frame received\n");
-            //return -1;
-        //}
-        }
+    capture.read(frame);
+    imshow("display", frame);
+    moveWindow("display", frame_size.width+75, 0);
+    imshow("edges from cvCanny", edges);
+    moveWindow("edges from cvCanny", frame_size.width+75, 75);
+    key_pressed = waitKey(20);
 
-     cvPyrDown(frame, half_frame, CV_GAUSSIAN_5x5); // Reduce the image by 2
-     //cvCvtColor(temp_frame, grey, CV_BGR2GRAY); // convert to grayscale
+    //double fx=0, double fy=0, int interpolation=INTER_LINEAR
+    resize(frame, half_frame, half_frame.size()); // Reduce the image by 2
 
-     // we're interested only in road below horizon - so crop top image portion off
-     //crop(IplImage* frame, temp_frame, cvRect(0,frame_size.height,frame_size.width,frame_size.height));
-     cvCvtColor(temp_frame, grey, CV_BGR2GRAY); // convert to grayscale
+    //cv::pyrDown(frame, half_frame, half_frame.size(), CV_GAUSSIAN_5x5); // Reduce the image by 2
 
-     // Perform a Gaussian blur ( Convolving with 5 X 5 Gaussian) & detect edges
-     cvSmooth(grey, grey, CV_GAUSSIAN, 5, 5);
-     cvCanny(grey, edges, CANNY_MIN_TRESHOLD, CANNY_MAX_TRESHOLD);
+    // we're interested only in road below horizon - so crop top image portion off
+    crop(frame, temp_frame, cvRect(0,frame_size.height,frame_size.width,frame_size.height/2));
+    cvtColor(temp_frame, grey, CV_BGR2GRAY); // convert to grayscale
 
-     // do Hough transform to find lanes
-     double rho = 1;
-     double theta = CV_PI/180;
-     CvSeq* lines = cvHoughLines2(edges, houghStorage, CV_HOUGH_PROBABILISTIC,
-         rho, theta, HOUGH_TRESHOLD, HOUGH_MIN_LINE_LENGTH, HOUGH_MAX_LINE_GAP);
+    // Perform a Gaussian blur ( Convolving with 5 X 5 Gaussian) & detect edges
+    //CV_GAUSSIAN_5x5.size;
+    int ksize=3;
+    double sigma=0.3*((ksize-1)*0.5 - 1) + 0.8;
+    int ktype=CV_32F;
+    Mat getGaussianKernel(ksize, sigma, ktype);
+    //Mat.getGaussianKernel(int ksize=3, double 0.3*((ksize-1)*0.5 - 1) + 0.8, CV_32F);
+    //cvSmooth (CvArr* grey, CvArr* grey, int smoothtype=CV_GAUSSIAN, int size1=3, int size2=0, double sigma1=0, double sigma2=0 );
+    Size kernel_size = getGaussianKernel.size();
+    GaussianBlur(grey, grey, kernel_size, 0, 0, BORDER_CONSTANT);
 
-     processLanes(lines, edges, temp_frame);
+    Canny(grey, edges, CANNY_MIN_TRESHOLD, CANNY_MAX_TRESHOLD);
+    //std::vector<Lane> lines;
 
-     // show middle line
-     cvLine(temp_frame, cvPoint(frame_size.width/2,0),
-     cvPoint(frame_size.width/2,frame_size.height), CV_RGB(255, 255, 0), 1);
+    // do Hough transform to find lanes
+    CvSeq* lines = HoughLinesP(edges, houghStorage, rho, theta, HOUGH_TRESHOLD, HOUGH_MIN_LINE_LENGTH, HOUGH_MAX_LINE_GAP);
 
-     cvShowImage("Grey", grey);
-     cvShowImage("Edges", edges);
-     cvShowImage("Color", temp_frame);
+    processLanes(lines, edges, temp_frame);
 
-     cvMoveWindow("Grey", 0, 0);
-     cvMoveWindow("Edges", 0, frame_size.height+25);
-     cvMoveWindow("Color", 0, 2*(frame_size.height+25));
+    // show middle line
+    cvLine(temp_frame, cvPoint(frame_size.width/2,0), cvPoint(frame_size.width/2,frame_size.height), CV_RGB(255, 255, 0), 1);
 
-     key_pressed = cvWaitKey(5);
+    cvShowImage("Grey", grey);
+    cvShowImage("Edges", edges);
+    cvShowImage("Color", temp_frame);
 
+    moveWindow("Grey", 0, 0);
+    moveWindow("Edges", 0, frame_size.height+25);
+    moveWindow("Color", 0, 2*(frame_size.height+25));
+}
+     //key_pressed = waitKey(5);
 
      cvReleaseMemStorage(&houghStorage);
-
 
      cvReleaseImage(&grey);
      cvReleaseImage(&edges);
      cvReleaseImage(&temp_frame);
-     cvReleaseImage(&half_frame);
-
+     half_frame.release();
      capture.release();
+     frame.release();
+     destroyAllWindows();
+     return 0;
 }
--- utils.h
+++ utils.h
@@ -36,3 +36,28 @@ public:
     }
 };
 
+CvPoint2D32f sub(CvPoint2D32f b, CvPoint2D32f a) { return cvPoint2D32f(b.x-a.x, b.y-a.y); }
+CvPoint2D32f mul(CvPoint2D32f b, CvPoint2D32f a) { return cvPoint2D32f(b.x*a.x, b.y*a.y); }
+CvPoint2D32f add(CvPoint2D32f b, CvPoint2D32f a) { return cvPoint2D32f(b.x+a.x, b.y+a.y); }
+CvPoint2D32f mul(CvPoint2D32f b, float t) { return cvPoint2D32f(b.x*t, b.y*t); }
+float dot(CvPoint2D32f a, CvPoint2D32f b) { return (b.x*a.x + b.y*a.y); }
+float dist(CvPoint2D32f v) { return sqrtf(v.x*v.x + v.y*v.y); }
+
+CvPoint2D32f point_on_segment(CvPoint2D32f line0, CvPoint2D32f line1, CvPoint2D32f pt){
+    CvPoint2D32f v = sub(pt, line0);
+    CvPoint2D32f dir = sub(line1, line0);
+    float len = dist(dir);
+    float inv = 1.0f/(len+1e-6f);
+    dir.x *= inv;
+    dir.y *= inv;
+
+    float t = dot(dir, v);
+    if(t >= len) return line1;
+    else if(t <= 0) return line0;
+
+    return add(line0, mul(dir,t));
+}
+
+float dist2line(CvPoint2D32f line0, CvPoint2D32f line1, CvPoint2D32f pt){
+    return dist(sub(point_on_segment(line0, line1, pt), pt));
+}
